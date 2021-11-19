9b43657ce96a3b891e559895565bad0f622c349f
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2021-11-19 12:31:27 -0700

    Horizontal plaform created and ghost wall corrected

---
+++ Assets/People/DBuckner/Scripts/HorizontalPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform left;
+    [SerializeField]
+    private Transform right;
+
+    bool goingLeft = true;
+
+    private void Update()
+    {
+        if(goingLeft)
+        {
+            transform.position -= new Vector3(1 * Time.deltaTime, 0, 0);
+            if (transform.position.x < left.position.x) goingLeft = false;
+        }
+        else
+        {
+            transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+            if (transform.position.x > right.position.x) goingLeft = true;
+        }
+    }
+
+}
